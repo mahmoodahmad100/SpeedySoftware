84f1db7617db5cefb47d663eae1210adffded970
mahmoodahmad100 <mahmood073@gmail.com>
2017-07-06 00:44:34 +0200

    Home page

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>speedy software | Home</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="css/custom.css">
+
+        <!--  FONTS  -->
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,300,900,700' rel='stylesheet' type='text/css'>
+        <!--  FONTS  -->
+
+    </head>
+    <body>
+    <div class="container">
+        
+        <!-- speedy software -->
+        <b class="title text-center">speedy software</b>
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                Cras blandit commodo vestibulum. Morbi volutpat pulvinar interdum. Aenean luctus nibh nec tempus dignissim. Etiam vestibulum metus ac placerat volutpat. Aliquam sed hendrerit justo. Sed faucibus justo quis aliquet tempus. Pellentesque volutpat viverra arcu a posuere. Phasellus aliquet consectetur felis, a bibendum ipsum consequat et. Donec venenatis dolor leo, nec dapibus tellus porta in. Sed laoreet aliquam magna, et varius arcu sollicitudin vel. Nunc ut nisl sapien. Pellentesque viverra dignissim nisi ac porta. Quisque eu massa imperdiet nisl vehicula faucibus. 
+                </p>
+            </div>
+        </div>
+        <!-- end speedy software -->
+
+        <!-- services -->
+        <b class="title text-center">services</b>
+        <div class="row">
+
+          <!--Responsive Websites col-->
+          <div class="col-md-4 col-sm-6 text-center">
+              <i class="fa fa-superpowers"></i>
+              <h3>Responsive Websites</h3>
+              <p>we can make responsive websites so that your website responsive to all devices small or big we can design the website the way you love</p>
+          </div>
+
+          <!--Design Websites col-->
+          <div class="col-md-4 col-sm-6 text-center">
+              <i class="fa fa-gift"></i>
+              <h3>Design Websites</h3>
+              <p>when we design a website we use the best tools to make your website looks amazing so we use html , css , jquery , bootstrap and javascript</p>
+          </div>
+
+          <!--Develop Websites col-->
+          <div class="col-md-4 col-sm-6 text-center">
+              <i class="fa fa-globe"></i>
+              <h3>Develop Websites</h3>
+              <p>as we work in the front-end we also work in the back-end in creating beautiful websites using the php as a language to develop and laravel as a framework</p>
+          </div>
+
+          <!--E-commerce Solution col-->
+          <div class="col-md-4 col-sm-6 text-center">
+              <i class="fa fa-money"></i>
+              <h3>E-commerce Solutions</h3>
+              <p>you want to sell your goods over the internet ? then here you can find what you want so we will bulid your online store using opencart platform</p>
+          </div>
+
+          <!--Graphic Designs col-->
+          <div class="col-md-4 col-sm-6 text-center">
+              <i class="fa fa-paint-brush"></i>
+              <h3>Graphic Designs</h3>
+              <p>an astonishing success comes with lure brand so here is the best place to design your logo , banner and advertisement professionally</p>
+          </div>
+
+          <!--Helpful Support col-->
+          <div class="col-md-4 col-sm-6 text-center">
+              <i class="fa fa-info-circle"></i>
+              <h3>Helpful Support</h3>
+              <p>do you stuck in trouble and don't know how to solve it ? here we can provide all the help you need to solve your problem and satisfy you</p>
+          </div>
+
+        </div>
+        <!-- end services -->
+
+    </div><!-- end container-->
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    </body>
+</html>
